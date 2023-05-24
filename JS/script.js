a06d2333f74f1e6f058400a45b4266281f1dece7
Dev Mishra <114310750+devm954@users.noreply.github.com>
2023-05-24 20:16:04 +0500

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger')
+const hamburgericon = document.querySelector('.hamburger i')
+const dropdownmenu = document.querySelector('.dropdown ')
+
+hamburger.onclick = function(){
+    dropdownmenu.classList.toggle('open')
+    const isopen = dropdownmenu.classList.contains('open')
+
+    hamburgericon.classList = isopen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars '
+}
